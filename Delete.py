40bf295f59f1b157900b91fd051e7a353eebc3b2
dgotlieb <Dgotlieb8@gmail.com>
2020-07-20 09:16:34 +0300

    Update Delete.py

--- Delete.py
+++ Delete.py
@@ -1,12 +1,14 @@
+import pymysql
+
 # Establishing a connection to DB
-conn = pymysql.connect(host='localhost', port=3306, user='root', passwd=‘123', db='table')
+conn = pymysql.connect(host='remotemysql.com', port=3306, user='root', passwd='123', db='SCHEMA_NAME')
 conn.autocommit(True)
 
 # Getting a cursor from Database
 cursor = conn.cursor()
 
 # Deleting data into table
-cursor.execute("DELETE FROM table.users WHERE name = ‘john'")
+cursor.execute("DELETE FROM table.users WHERE name = 'john'")
 
 cursor.close()
 conn.close()
