8234b3511bd7bc978aac7bb4a4302cafd75efc94
dgotlieb <Dgotlieb8@gmail.com>
2020-07-20 09:17:56 +0300

    Update Update.py

--- Update.py
+++ Update.py
@@ -1,12 +1,15 @@
+import pymysql
+
 # Establishing a connection to DB
-conn = pymysql.connect(host='localhost', port=3306, user='root', passwd=‘123', db='table')
+conn = pymysql.connect(host='remotemysql.com', port=3306, user='root', passwd='123', db='SCHEMA_NAME')
+
 conn.autocommit(True)
 
 # Getting a cursor from Database
 cursor = conn.cursor()
 
 # Updating data inside the table
-cursor.execute("UPDATE table.users SET id = '10' WHERE name = ‘john'")
+cursor.execute("UPDATE SCHEMA_NAME.users SET id = '10' WHERE name = 'john'")
 
 cursor.close()
 conn.close()
