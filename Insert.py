905f7854e7b49d2e1e4ff98412a6f54a2ba32b60
dgotlieb <Dgotlieb8@gmail.com>
2020-07-20 09:15:58 +0300

    Update Insert.py

--- Insert.py
+++ Insert.py
@@ -1,12 +1,14 @@
+import pymysql
+
 # Establishing a connection to DB
-conn = pymysql.connect(host='localhost', port=3306, user='root', passwd=‘123', db='table')
+conn = pymysql.connect(host='remotemysql.com', port=3306, user='root', passwd='123', db='SCHEMA_NAME')
 conn.autocommit(True)
 
 # Getting a cursor from Database
 cursor = conn.cursor()
 
 # Inserting data into table
-cursor.execute("INSERT into table.users (name, id) VALUES (‘john', 5)")
+cursor.execute("INSERT into SCHEMA_NAME.users (name, id) VALUES (‘john', 5)")
 
 cursor.close()
 conn.close()
