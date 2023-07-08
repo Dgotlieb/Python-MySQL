264c2d314c57a8beab7d86981cbf1a8ae9915905
dgotlieb <Dgotlieb8@gmail.com>
2023-07-08 18:20:43 +0300

    Update and rename Select.py to select_data.py

--- select_data.py
+++ select_data.py
@@ -1,13 +1,14 @@
 import pymysql
 
-# Establishing a connection to DB
-conn = pymysql.connect(host='remotemysql.com', port=3306, user='root', passwd='123', db='SCHEMA_NAME')
+schema_name = "mydb"
 
+# Establishing a connection to DB
+conn = pymysql.connect(host='127.0.0.1', port=3306, user='user', passwd='password', db=schema_name)
 # Getting a cursor from Database
 cursor = conn.cursor()
 
 # Getting all data from table “users”
-cursor.execute("SELECT * FROM SCHEMA_NAME.users;")
+cursor.execute(f"SELECT * FROM {schema_name}.users;")
 
 # Iterating table and printing all users
 for row in cursor:
